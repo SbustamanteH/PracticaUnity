941c1ce8d4c53c52fefbc4be1664b3c3c410c193
unknown <sbustamante@uabc.edu.mx>
2018-09-07 19:18:46 -0700

    Avance Practica 2 y 3

---
+++ Practicas_Unity/Assets/Scripts/Interactable.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour 
+{
+    public Color color;
+    public GameObject infoPanel;
+    private bool isPlayerInside = false;
+    public float rotationSpeed = 3f;
+
+
+    private void Start()
+    {
+        infoPanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+      
+
+        if (Input.GetKeyDown(KeyCode.E) && isPlayerInside)
+        {
+            gameObject.GetComponent<Renderer>().material.color = color;
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("Player entro", gameObject);
+            infoPanel.SetActive(true);
+            isPlayerInside = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            infoPanel.SetActive(false);
+            isPlayerInside = false;
+        }
+    }
+}
